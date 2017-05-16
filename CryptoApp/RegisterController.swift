4be1015524930e752b6114eda3b30758f5eae65d
stagiaire <stagiaire@iMac12.local>
2017-05-16 11:40:46 +0200

    [update] debut de mise en place requete sur API

---
+++ CryptoApp/RegisterController.swift
@@ -0,0 +1,44 @@
+//
+//  RegisterController.swift
+//  CryptoApp
+//
+//  Created by stagiaire on 16/05/2017.
+//  Copyright © 2017 guinicarji. All rights reserved.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var btn_register: UIButton!
+    @IBOutlet weak var password_confirmation: UITextField!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnRegister(_ sender: UIButton) {
+        if ( password.text == password_confirmation.text){
+            if(password.text != nil){
+                print("\(String(describing: password.text))")
+            }
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
